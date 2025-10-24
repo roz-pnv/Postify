b6e3f70368299af564c9213a26934f713a2db485
roz-pnv <rozhinpanaw@gmail.com>
2025-10-24 07:10:51 +0330

    style(code): remove extra blank lines between namespace and imports
    Clean up `Stream.php` and `Uri.php` by eliminating unnecessary empty
    lines between the namespace declaration and `use` statements.

--- web/app/src/Http/Requests/Stream.php
+++ web/app/src/Http/Requests/Stream.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-
 use Exception;
 use InvalidArgumentException;
 
--- web/app/src/Http/Requests/Uri.php
+++ web/app/src/Http/Requests/Uri.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-
 use Psr\Http\Message\UriInterface;
 
 class Uri implements UriInterface
